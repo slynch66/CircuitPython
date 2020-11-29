ed045cd0726153bd456158866a59ef5f0338ce79
slynch66 <55889829+slynch66@users.noreply.github.com>
2020-11-29 08:49:44 -0500

    Update CircuitPython_Servo.py

--- CircuitPython_Servo.py
+++ CircuitPython_Servo.py
@@ -5,23 +5,23 @@ import pulseio
 from adafruit_motor import servo
  
  
-angle = 179    # creates
+angle = 179    # creates the variable for the servo angle
  
 touch_pad = board.A0     # "touch_pad" is a variable for the capacitive touch and here it is assigned to pin 0
 touch_pad1 = board.A1
 
-touch = touchio.TouchIn(touch_pad)    # TouchIn is reading the state of the capacitive touch
+touch = touchio.TouchIn(touch_pad)    # TouchIn is reading the state of the capacitive touch, similar to digitalRead() for Arduino
 touch1 = touchio.TouchIn(touch_pad1)
  
 # create a PWMOut object on Pin A2.
 pwm = pulseio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
  
-# Create a servo object, my_servo.
+# Finish coding in the servo
 my_servo = servo.Servo(pwm)
  
 
-while True:
-    if touch.value and not touch1.value and angle < 180:
+while True:     # This while True statement is saying if I only touch one of the wires, move the servo that way
+    if touch.value and not touch1.value and angle < 180:   
         angle += 1
     if touch1.value and not touch.value and angle > 0:
         angle -= 1
