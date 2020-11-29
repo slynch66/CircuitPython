991cf7bd33f78cd4a2449d9b89e5db381e60d9d8
slynch66 <55889829+slynch66@users.noreply.github.com>
2020-11-29 08:44:31 -0500

    Create Hello_CircuitPython.py

---
+++ Hello_CircuitPython.py
@@ -0,0 +1,10 @@
+```python
+import board
+import neopixel
+
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+print("Make it red!")           # Makes the serial monitor say "Make it red!" when the LED is red.
+while True:                  # While True is basically the Loop command in Arduino.
+dot.fill((255,0,0))     #This command makes the neopixel LED on the Metroid light up red.
+```
